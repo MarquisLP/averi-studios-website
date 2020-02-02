4e7d322a6bfccfa54805a5a5fb80db01603802d0
Mark Padilla <markjjpadilla@gmail.com>
2020-02-01 21:45:07 -0500

    Display company name and slogan at top of website

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
+import React from 'react'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import Intro from './Intro'
+
+const appTheme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#27a4f3',
+      contrastText: '#fff'
+    }
+  }
+})
+
 function App () {
   return (
-    null
+    <ThemeProvider theme={appTheme}>
+      <Intro />
+    </ThemeProvider>
   )
 }
 
---
+++ src/Intro.js
@@ -0,0 +1,44 @@
+import React from 'react'
+import { makeStyles } from '@material-ui/styles'
+import Typography from '@material-ui/core/Typography'
+import { Box } from '@material-ui/core'
+
+const useStyles = makeStyles({
+  content: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: '30vh'
+  }
+})
+
+function Intro () {
+  const classes = useStyles()
+
+  return (
+    <div
+      className={classes.content}
+    >
+      <Typography
+        variant='h1'
+        color='primary'
+      >
+        <Box
+          fontWeight='fontWeightBold'
+          fontSize='2em'
+        >
+          Averi Studios
+        </Box>
+      </Typography>
+      <Typography
+        variant='h3'
+        color='primary'
+      >
+        Apps as clear as blue skies
+      </Typography>
+    </div>
+  )
+}
+
+export default Intro
