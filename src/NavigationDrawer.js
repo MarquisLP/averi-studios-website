3d0c4508a7734dcbf4706bb82825b31f03a10d7a
Mark Padilla <markjjpadilla@gmail.com>
2020-02-03 22:39:09 -0500

    Implement navigation drawer to enhance aesthetics and usability

---
+++ src/NavigationDrawer.js
@@ -0,0 +1,92 @@
+import React from 'react'
+import { Drawer, List, ListItem, ListItemText, makeStyles, ListItemIcon } from '@material-ui/core'
+import { EmojiPeople as EmojiPeopleIcon, Apps as AppsIcon, Announcement as AnnouncementIcon, Email as EmailIcon } from '@material-ui/icons'
+
+function NavigationDrawer (props) {
+  const useStyles = makeStyles({
+    drawer: {
+      width: props.drawerWidth,
+      flexShrink: 0
+    },
+    drawerPaper: {
+      width: props.drawerWidth,
+      background: '#27a4f3',
+      color: 'white',
+      boxShadow: '0px 0px 12px black'
+    }
+  })
+  const classes = useStyles()
+
+  return (
+    <Drawer
+      className={classes.drawer}
+      classes={{
+        paper: classes.drawerPaper
+      }}
+      open={props.isOpen}
+      variant='persistent'
+      anchor='left'
+    >
+      <List>
+        <ListItem
+          button
+          onClick={() => props.scrollToRef(props.sectionRefs.intro)}
+          key='intro'
+        >
+          <ListItemIcon>
+            <EmojiPeopleIcon
+              color='secondary'
+            />
+          </ListItemIcon>
+          <ListItemText
+            primary='Intro'
+          />
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => props.scrollToRef(props.sectionRefs.apps)}
+          key='apps'
+        >
+          <ListItemIcon>
+            <AppsIcon
+              color='secondary'
+            />
+          </ListItemIcon>
+          <ListItemText
+            primary='Apps'
+          />
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => props.scrollToRef(props.sectionRefs.announcements)}
+          key='announcements'
+        >
+          <ListItemIcon>
+            <AnnouncementIcon
+              color='secondary'
+            />
+          </ListItemIcon>
+          <ListItemText
+            primary='Announcements'
+          />
+        </ListItem>
+        <ListItem
+          button
+          onClick={() => props.scrollToRef(props.sectionRefs.footer)}
+          key='contact'
+        >
+          <ListItemIcon>
+            <EmailIcon
+              color='secondary'
+            />
+          </ListItemIcon>
+          <ListItemText
+            primary='Contact'
+          />
+        </ListItem>
+      </List>
+    </Drawer>
+  )
+}
+
+export default NavigationDrawer
--- src/Root.js
+++ src/Root.js
@@ -1,9 +1,13 @@
-import React from 'react'
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles'
+import React, { useRef } from 'react'
+import { createMuiTheme, ThemeProvider, makeStyles } from '@material-ui/core/styles'
 import Intro from './Intro'
 import Apps from './apps/Apps'
 import Announcements from './announcements/Announcements'
 import Footer from './Footer'
+import NavigationDrawer from './NavigationDrawer'
+import clsx from 'clsx'
+
+const drawerWidth = 240
 
 const appTheme = createMuiTheme({
   palette: {
@@ -11,19 +15,77 @@ const appTheme = createMuiTheme({
       main: '#27a4f3',
       contrastText: '#fff'
     },
+    secondary: {
+      main: '#ffffff'
+    },
     text: {
       secondary: '#ffffff'
     }
   }
 })
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex',
+    margin: 0,
+    padding: 0
+  },
+  content: {
+    flexGrow: 1
+  }
+}))
+
+const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)
+
 function Root () {
+  const classes = useStyles()
+  const sectionRefs = {
+    intro: useRef(null),
+    apps: useRef(null),
+    announcements: useRef(null),
+    footer: useRef(null)
+  }
+
+  let drawerIsOpen = true
+
+  function setDrawerOpenState (isOpen) {
+    drawerIsOpen = isOpen
+  }
+
   return (
     <ThemeProvider theme={appTheme}>
-      <Intro />
-      <Apps />
-      <Announcements />
-      <Footer />
+      <div
+        className={classes.root}
+      >
+        <NavigationDrawer
+          drawerWidth={drawerWidth}
+          isOpen={drawerIsOpen}
+          setOpenState={setDrawerOpenState}
+          sectionRefs={sectionRefs}
+          scrollToRef={scrollToRef}
+        />
+        <main
+          className={classes.content}
+          ref={sectionRefs.intro}
+        >
+          <Intro />
+          <div
+            ref={sectionRefs.apps}
+          >
+            <Apps />
+          </div>
+          <div
+            ref={sectionRefs.announcements}
+          >
+            <Announcements />
+          </div>
+          <div
+            ref={sectionRefs.footer}
+          >
+            <Footer />
+          </div>
+        </main>
+      </div>
     </ThemeProvider>
   )
 }
